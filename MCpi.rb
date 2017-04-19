446a4c626c09c33a196579a61734994617a7fc31
Seiya Fukushima <Seiya Fukushima>
2017-04-19 18:10:30 +0900

    pie-pi

---
+++ MCpi.rb
@@ -0,0 +1,16 @@
+num = gets().to_i
+n=0
+p=0
+
+while n < num do
+    x = rand
+    y = rand
+    point = x*x + y*y
+    if point <= 1
+        p += 1
+    end
+    n += 1
+end
+
+pi = p.to_f/n*4
+puts pi
